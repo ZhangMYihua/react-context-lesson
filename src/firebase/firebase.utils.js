1e9b5c970d30620d038b586c60631e03290018b9
Roman Kolerov <coder8080@mail.ru>
2022-04-27 14:00:56 +0300

    Updated dependencies, fixed installation and startup errors

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 const config = {
   apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
@@ -9,7 +9,7 @@ const config = {
   projectId: 'crwn-db',
   storageBucket: 'crwn-db.appspot.com',
   messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  appId: '1:850995411664:web:7ddc01d597846f65',
 };
 
 firebase.initializeApp(config);
@@ -29,7 +29,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
       console.log('error creating user', error.message);
